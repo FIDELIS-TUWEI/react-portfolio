415405be551f9c6af1f355b3544fee829244a87f
Fidelis <fideliofidel9@gmail.com>
2023-02-11 12:46:58 +0300

    Header component rendered / CTA rendered

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import CTA from './CTA'
 import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm</h5>
+        <h1>Fidelis Tuwei</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
