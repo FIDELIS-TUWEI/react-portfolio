cdb10b5bb9b3fc6d7d25c6e5dd65f92dae92bb60
Fidelis <fideliofidel9@gmail.com>
2023-02-13 08:11:50 +0300

    Testimonial / css done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './testimonials.css'
+import AVATAR1 from '../../assests/avatar1.jpg'
+import AVATAR2 from '../../assests/avatar2.jpg'
+import AVATAR3 from '../../assests/avatar3.jpg'
+import AVATAR4 from '../../assests/avatar4.jpg'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR1} alt="" />
+          </div>
+
+          <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque vitae architecto animi et, earum consequuntur fugit debitis aspernatur nostrum quisquam nemo suscipit, mollitia molestiae id nisi error? Eum, odio eos.
+            </small>
+        </article>
+      </div>
+    </section>
   )
 }
 
